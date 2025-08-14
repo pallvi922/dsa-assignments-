50f14b872f6501c184a0ca86ff40fb10131fef31
pallvi922 <ppallvi_be24@thapar.edu>
2025-08-14 12:04:00 +0530

    ques2.cpp

---
+++ assignment 1/ques2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int newSize = n;
+    for(int i = 0; i < newSize; i++) {
+        for(int j = i + 1; j < newSize; j++) {
+            if(arr[i] == arr[j]) {
+                for(int k = j; k < newSize - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                newSize--;
+                j--;
+            }
+        }
+    }
+
+    cout << "Array after removing duplicates: ";
+    for(int i = 0; i < newSize; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
